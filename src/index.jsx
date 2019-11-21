b32e203c55dfda40c6b301a904cd3a4be12af865
Tayler Johnson <johnsontayler@me.com>
2019-11-21 16:00:12 -0300

    bootstrap redux app

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,24 @@
+//React
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import '../assets/stylesheets/application.scss';
 import App from './components/app'
 
+//Redux
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+import gifReducer from './reducers/flats_reducer';
+
+const reducers = combineReducers({
+  gif: gifReducer
+});
+
+//Render
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<App />, root);
+  ReactDOM.render(
+    <Provider store={createStore(reducers)}>
+      <App />
+    </Provider>, root
+  );
 }
