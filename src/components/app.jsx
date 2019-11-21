5e710f6cc005ba3ac04c1e60b89bc6e7c0d6d7f4
Tayler Johnson <johnsontayler@me.com>
2019-11-21 11:22:10 -0300

    left scene info text added

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -9,9 +9,17 @@ class App extends Component {
         <div className="banner">
           <h2>Weirdness Calculator</h2>
         </div>
-        <div className="left-scene">
-        </div>
-        <div className="right-scene">
+        <div className="main-content">
+          <div className="left-scene">
+            <p>Find out how weird you are by selecting the GIFs that make
+              you laugh. We'll show you the latest weird ones to start,
+              but you can move the slider to make them weirder.</p>
+            <p>When you find a GIF that you like, press the <i>Like</i> button.
+              Once you like 5 GIFs, we'll show you how weird you
+              are.</p>
+          </div>
+          <div className="right-scene">
+          </div>
         </div>
       </div>
     );
