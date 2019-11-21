b556e9fee6e06701db687457f7f34b423b0d4a93
Tayler Johnson <johnsontayler@me.com>
2019-11-21 12:52:39 -0300

    search container+styling, and gif result container+styling

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SearchBar from './search_bar';
+import Gif from '../containers/gif'
 
 class App extends Component {
 
@@ -12,13 +13,24 @@ class App extends Component {
         </div>
         <div className="main-content">
           <div className="left-scene">
-            <p>Find out how weird you are by selecting the GIFs that make
-              you laugh. We'll show you the latest weird ones to start,
+            <div className="search-container">
+              <p>Find out how weird you are by selecting the GIFs that make
+                you laugh. We'll show you the latest weird ones to start,
               but you can move the slider to make them weirder.</p>
-            <p>When you find a GIF that you like, press the <i>Like</i> button.
-              Once you like 5 GIFs, we'll show you how weird you
+              <p>When you find a GIF that you like, press the <i>Like</i> button.
+                Once you like 5 GIFs, we'll show you how weird you
               are.</p>
-            <SearchBar />
+              <SearchBar />
+            </div>
+            <div className="gif-result">
+              <h3>YOUR RESULT</h3>
+              <Gif />
+              <br /><br />
+              <button className="btn btn-primary btn-like my-2 my-sm-0"
+                type="submit">
+                <i className="fas fa-thumbs-up"></i>
+              </button>
+            </div>
           </div>
           <div className="right-scene">
           </div>
