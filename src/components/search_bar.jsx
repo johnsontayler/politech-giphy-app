8219137d84091491fd4e4eb587704649e4acdf2b
Tayler Johnson <johnsontayler@me.com>
2019-11-21 16:37:00 -0300

    search_bar ESlint

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -4,15 +4,19 @@ class SearchBar extends Component {
   render() {
     const style = {
       borderRadius: 0
-    }
+    };
 
     return (
       <form className="form-inline my-2 my-lg-0">
         <p>Search term</p>
-        <input className="form-control" style={style} type="search"
-          placeholder="Hamburger" aria-label="Search" />
-        <button className="btn btn-primary my-2 my-sm-0" style={style}
-          type="submit">Search</button>
+        <input className="form-control"
+          style={style} type="search" placeholder="Hamburger"
+          aria-label="Search"
+        />
+        <button className="btn btn-primary my-2 my-sm-0"
+          style={style} type="submit">
+          Search
+        </button>
       </form>
     );
   }
