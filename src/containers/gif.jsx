66e2756b9b2f393cbca123636722c84bcc241656
Tayler Johnson <johnsontayler@me.com>
2019-11-21 16:19:23 -0300

    gif connect state/dispatch to props

--- src/containers/gif.jsx
+++ src/containers/gif.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
-
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { setGif } from '../actions';
 class Gif extends Component {
+
+  componentWillMount() {
+    this.props.setGif();
+  }
+
   render() {
     const src = `https://media.giphy.com/media/${this.props.id}/giphy.gif`
 
@@ -10,4 +17,17 @@ class Gif extends Component {
   }
 }
 
-export default Gif;
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    { setGif: setGif },
+    dispatch
+  );
+}
+
+function mapStateToProps(state) {
+  return {
+    gif: state.gif
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Gif);
