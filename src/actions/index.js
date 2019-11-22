59fd5edc48b4ce56c750ac9c54ec6d8e6c8559c4
Tayler Johnson <johnsontayler@me.com>
2019-11-22 02:45:35 -0300

    optional variables setGif

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,20 +1,18 @@
 export const SET_GIF = 'SET_GIF';
 
 export function submitTerm(value) {
-  setGif(value.search)
+  setGif(value.search);
 }
 
-export function setGif(value = "Hamburger") {
-  let string = value
+export function setGif(string = "Kanye", weirdness = 10) {
   console.log(string);
-  let weirdness = 0;
   const GIPHY_API_KEY = 'dFHHRwtfNPxLFWUitpW96f1QWdm6AceW';
   const url = `http://api.giphy.com/v1/gifs/translate?s=${string}?weirdness=${weirdness}&api_key=${GIPHY_API_KEY}`
-
+  console.log(url);
   const promise = fetch(url).then(response => response.json());
 
   return {
     type: SET_GIF,
     payload: promise
-  }
+  };
 }
