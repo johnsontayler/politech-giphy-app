632da2d5d935560d0718d84022dd1c247b32e96d
Tayler Johnson <johnsontayler@me.com>
2019-11-22 02:45:04 -0300

    class searchBar

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,33 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 import { submitTerm } from '../actions';
 
-let SearchBar = (props) => {
-  const { handleSubmit } = props;
-  const style = { borderRadius: 0 };
+class SearchBar extends Component {
+  render() {
+    const { handleSubmit } = this.props;
+    const style = { borderRadius: 0 };
 
-  return (
-    <form className="form-inline my-2 my-lg-0"
-      onSubmit={handleSubmit(submitTerm)}>
-      <p>Search term</p>
-      <Field className="form-control"
-        component="input"
-        name="search"
-        placeholder="Hamburger"
-        type="text"
-        style={style}
-        aria-label="Search"
-      />
-      <button className="btn btn-primary my-2 my-sm-0"
-        style={style}
-        type="submit">
-        Search
-      </button>
-    </form>
-  );
+    return (
+      <form className="form-inline my-2 my-lg-0"
+        onSubmit={handleSubmit(submitTerm)}>
+        <p>Search term</p>
+        <Field className="form-control"
+          component="input"
+          name="search"
+          placeholder="Hamburger"
+          type="text"
+          style={style}
+          aria-label="Search"
+        />
+        <button className="btn btn-primary my-2 my-sm-0"
+          style={style}
+          type="submit">
+          Search
+        </button>
+      </form>
+    );
+  }
 }
 
 SearchBar = reduxForm({
