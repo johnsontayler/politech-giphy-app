ddfa4c100747588b00c5d604f3e2ceb5d5d1a2cb
Tayler Johnson <johnsontayler@me.com>
2019-11-21 23:31:23 -0300

    redux form, pass prop value to action

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,25 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { reduxForm, Field } from 'redux-form';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-class SearchBar extends Component {
-  render() {
-    const style = {
-      borderRadius: 0
-    };
+import { submitTerm } from '../actions';
 
-    return (
-      <form className="form-inline my-2 my-lg-0">
-        <p>Search term</p>
-        <input className="form-control"
-          style={style} type="search" placeholder="Hamburger"
-          aria-label="Search"
-        />
-        <button className="btn btn-primary my-2 my-sm-0"
-          style={style} type="submit">
-          Search
-        </button>
-      </form>
-    );
-  }
+let SearchBar = (props) => {
+  const { handleSubmit } = props;
+  const style = { borderRadius: 0 };
+
+  return (
+    <form className="form-inline my-2 my-lg-0"
+      onSubmit={handleSubmit(submitTerm)}>
+      <p>Search term</p>
+      <Field className="form-control"
+        component="input"
+        name="search"
+        placeholder="Hamburger"
+        type="text"
+        style={style}
+        aria-label="Search"
+      />
+      <button className="btn btn-primary my-2 my-sm-0"
+        style={style}
+        type="submit">
+        Search
+      </button>
+    </form>
+  );
 }
 
-export default SearchBar;
+SearchBar = reduxForm({
+  form: 'SearchBar'
+})(SearchBar)
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    { submitTerm: submitTerm },
+    dispatch
+  );
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
