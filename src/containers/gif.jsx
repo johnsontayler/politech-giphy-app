cb42ba136287fbaf1ff61215db4911a34886328a
Tayler Johnson <johnsontayler@me.com>
2019-11-21 23:34:25 -0300

    reformat

--- src/containers/gif.jsx
+++ src/containers/gif.jsx
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+
 import { setGif } from '../actions';
 
 class Gif extends Component {
+
   componentWillMount() {
     this.props.setGif();
   }
 
   render() {
+    // console.log(this.props.gif)
     const src = `https://media.giphy.com/media/${this.props.id}/giphy.gif`;
 
     return (
