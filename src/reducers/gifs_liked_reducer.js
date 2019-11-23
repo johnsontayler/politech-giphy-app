96ef1e81d384d672bab8fedabd50df6fe696b934
Tayler Johnson <johnsontayler@me.com>
2019-11-23 15:33:03 -0300

    default count 5, remove 1 on like action

--- src/reducers/gifs_liked_reducer.js
+++ src/reducers/gifs_liked_reducer.js
@@ -1,10 +1,18 @@
 import { LIKE_GIF, REMOVE_GIF } from '../actions';
 
-export default function (prevState = [], action) {
+export default function (
+  prevState = {
+    gifs: [],
+    count: 5
+  }, action) {
 
   switch (action.type) {
     case LIKE_GIF:
-      return prevState.concat(action.gifLiked)
+      return {
+        prevState,
+        gifs: prevState.gifs.concat(action.gifs),
+        count: prevState.count - action.count
+      }
     case REMOVE_GIF:
       return prevState.filter((gif, index) => index != action.index)
     default:
