c05f144948fd43bc6360c092de2561f5efb054a1
Tayler Johnson <johnsontayler@me.com>
2019-11-24 02:22:44 -0300

    addweird action type to weirdness reducer

--- src/scenes/Home/services/setWeirdness/reducer.js
+++ src/scenes/Home/services/setWeirdness/reducer.js
@@ -1,7 +1,8 @@
-import { SET_WEIRDNESS } from './actions';
+import { SET_WEIRDNESS, ADD_WEIRDNESS } from './actions';
 
 const initialState = {
   number: 0,
+  score: 0,
 }
 
 export default function (prevState = initialState, action) {
@@ -9,6 +10,8 @@ export default function (prevState = initialState, action) {
   switch (action.type) {
     case SET_WEIRDNESS:
       return { ...prevState, number: action.number }
+    case ADD_WEIRDNESS:
+      return { ...prevState, score: action.gifWeirdness }
     default:
       return prevState;
   }
