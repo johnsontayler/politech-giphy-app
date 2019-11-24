53d644b0103487be69a1bfac40a32e2b40fc1ae0
Tayler Johnson <johnsontayler@me.com>
2019-11-23 21:33:36 -0300

    slider html

---
+++ src/scenes/Home/containers/WeirdnessSlider/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class WeirdnessSlider extends Component {
+
+  render() {
+    return (
+      <div id="weirdnessslider" class="weirdness-slider">
+        <div class="thumb"></div>
+      </div>
+    );
+  }
+}
+
+export default WeirdnessSlider;
+
