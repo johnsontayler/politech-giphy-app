508df825e828142a8532aecf54eedcb1632314c7
Tayler Johnson <johnsontayler@me.com>
2019-11-24 02:38:16 -0300

    add weirdness key to gif result

--- src/scenes/Home/services/setGifs/reducer.js
+++ src/scenes/Home/services/setGifs/reducer.js
@@ -2,6 +2,7 @@ import { SET_GIF, LIKE_GIF, UNLIKE_GIF } from './actions';
 
 const initialState = {
   result: [],
+  weirdness: 0,
   liked: [],
   likedCount: 0
 }
@@ -10,7 +11,11 @@ export default function (prevState = initialState, action) {
 
   switch (action.type) {
     case SET_GIF:
-      return { ...prevState, result: action.payload.data }
+      return {
+        ...prevState,
+        result: action.payload.data,
+        weirdness: action.weirdness
+      }
     case LIKE_GIF:
       return {
         ...prevState,
