349844212874b832f13c1d17d19bbc3f2b8181fc
Tayler Johnson <johnsontayler@me.com>
2019-11-23 23:58:25 -0300

    changeWeirdness action bound to slider component

--- src/scenes/Home/containers/WeirdnessSlider/index.jsx
+++ src/scenes/Home/containers/WeirdnessSlider/index.jsx
@@ -1,15 +1,36 @@
+// External modules
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+// Actions
+import { changeWeirdness } from '../../services/setWeirdness/actions';
 
 class WeirdnessSlider extends Component {
 
+  handleSlide = (event) => {
+    event.preventDefault();
+    this.props.changeWeirdness(event);
+  }
+
   render() {
     return (
-      <div id="weirdnessslider" className="weirdness-slider">
-        <div className="thumb"></div>
+      <div id="weirdnessSlider" className="weirdness-slider">
+        <div className="thumb"
+          onMouseDown={this.handleSlide}
+          onDragStart={() => false}>
+        </div>
       </div>
     );
   }
 }
 
-export default WeirdnessSlider;
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    { changeWeirdness: changeWeirdness },
+    dispatch
+  );
+}
+
+export default connect(null, mapDispatchToProps)(WeirdnessSlider);
 
