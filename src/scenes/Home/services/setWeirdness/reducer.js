b5a6af56886d2f4acd9c31bf66eec19e5e06edaa
Tayler Johnson <johnsontayler@me.com>
2019-11-24 04:25:30 -0300

    keep array of weirdness scores

--- src/scenes/Home/services/setWeirdness/reducer.js
+++ src/scenes/Home/services/setWeirdness/reducer.js
@@ -1,8 +1,8 @@
-import { SET_WEIRDNESS, ADD_WEIRDNESS } from './actions';
+import { SET_WEIRDNESS, ADD_SCORE, SUBTRACT_SCORE } from './actions';
 
 const initialState = {
   number: 0,
-  score: 0,
+  score: [],
 }
 
 export default function (prevState = initialState, action) {
@@ -10,11 +10,16 @@ export default function (prevState = initialState, action) {
   switch (action.type) {
     case SET_WEIRDNESS:
       return { ...prevState, number: action.number }
-    case ADD_WEIRDNESS:
+    case ADD_SCORE:
       return {
         ...prevState,
-        score: prevState.score + action.gifWeirdness
+        score: prevState.score.concat(action.gifWeirdness)
       }
+    // case SUBTRACT_SCORE:
+    //   return {
+    //     ...prevState,
+    //     score: prevState.score - action.gifWeirdness
+    //   }
     default:
       return prevState;
   }
