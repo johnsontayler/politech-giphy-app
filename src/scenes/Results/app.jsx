5ca402aa851a3c86d5e0060d6eb8702ad579dddb
Tayler Johnson <johnsontayler@me.com>
2019-11-25 13:52:48 -0300

    toggle conffetti on results mount and restart click, weirdnessresults container added

--- src/scenes/Results/app.jsx
+++ src/scenes/Results/app.jsx
@@ -1,32 +1,42 @@
 //  External modules
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 //  Components+Containers
 import GifConfetti from './components/GifConfetti';
 import Banner from '../../components/Banner';
-import GifsLikedResult from './containers/GifsLikedResult';
-import WeirdnessScore from './containers/WeirdnessScore';
+import WeirdnessResults from './containers/WeirdnessResults';
+
+//  Actions
+import { toggleConfetti } from './services/setResults/actions';
 
 class App extends Component {
+
+  componentDidMount() {
+    if (this.props.results) {
+      setTimeout(() => this.props.toggleConfetti(), 500);
+    }
+  }
+
   render() {
+    const { gifsLiked } = this.props
+
     return (
       <div>
         <GifConfetti />
         <Banner />
         <div className="results">
-          <WeirdnessScore />
-          <div className="final-gifs-liked">
-            <p style={{ textAlign: "left" }}>
-              <strong>The GIFs you liked</strong>
-            </p>
-            <GifsLikedResult />
-          </div>
-          <Link to="/">
+          <WeirdnessResults />
+          <Link to={{
+            pathname: "/",
+            state: { gifsLiked: gifsLiked }
+          }}>
             <button
               className="btn btn-primary my-2 my-sm-0"
               type="submit"
-              onClick={this.clearGifsLiked}
+              onClick={() => this.props.toggleConfetti()}
               style={{ borderRadius: 2 }}
             >
               START OVER
@@ -38,4 +48,20 @@ class App extends Component {
   }
 }
 
-export default App;
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    {
+      toggleConfetti: toggleConfetti
+    },
+    dispatch
+  );
+}
+
+function mapStateToProps(state) {
+  return {
+    gifsLiked: state.gifs.liked,
+    results: state.results
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
