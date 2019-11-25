13aa2207985275d22f0883d3006ee6b8d35a998b
Tayler Johnson <johnsontayler@me.com>
2019-11-25 01:29:15 -0300

    restructure folders

--- src/scenes/Home/components/ButtonUnlike/index.jsx
+++ src/scenes/Home/components/ButtonUnlike/index.jsx
@@ -1,37 +1,17 @@
 //  External modules
-import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
-// Actions
-import { unlikeGif } from '../../services/setGifs/actions';
-import { subtractScore } from '../../services/setWeirdness/actions';
+const ButtonUnlike = (props) => {
+  const { handleClick } = props
 
-class ButtonUnlike extends Component {
-  handleRemove = (e) => {
-    e.preventDefault();
-    console.log(this.props.gifIndex);
-    this.props.unlikeGif(this.props.gifIndex);
-    this.props.subtractScore(this.props.gifIndex);
-  };
-
-  render() {
-    return (
-      <FontAwesomeIcon icon={faTimesCircle}
-        className="fa-times-circle"
-        onClick={this.handleRemove}
-      />
-    );
-  }
-}
-
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators(
-    { unlikeGif: unlikeGif, subtractScore: subtractScore },
-    dispatch
+  return (
+    <FontAwesomeIcon icon={faTimesCircle}
+      className="fa-times-circle"
+      onClick={handleClick}
+    />
   );
 }
 
-export default connect(null, mapDispatchToProps)(ButtonUnlike);
+export default ButtonUnlike;
---
+++ src/scenes/Home/containers/WeirdnessCalculator/index.jsx
@@ -0,0 +1,81 @@
+//  External modules
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+//  Components
+import GifsLiked from '../../components/GifsLiked';
+import Button from '../../../../components/Button';
+import GifsLikedCounter from '../../components/GifsLikedCounter';
+
+// Actions
+import { unlikeGif } from '../../services/setGifs/actions';
+import { subtractScore } from '../../services/setWeirdness/actions';
+import { calculateScore } from '../../services/setWeirdness/actions';
+
+class WeirdnessCalculator extends Component {
+
+  handleUnlikeGif = (gifIndex) => {
+    this.props.unlikeGif(gifIndex);
+    this.props.subtractScore(gifIndex);
+  };
+
+  handleCalculation = () => {
+    if (this.props.gifsLikedCount < 5) {
+      alert("You must like 5 GIFs to calculate your score.")
+    } else {
+      this.props.calculateScore();
+    }
+  };
+
+  render() {
+    const { gifsLiked, gifsLikedCount } = this.props;
+
+    let resultsPath = "/results";
+    if (gifsLikedCount < 5) resultsPath = "/";
+
+    return (
+      <div>
+        <GifsLiked
+          gifsLiked={gifsLiked}
+          unlikeGif={this.handleUnlikeGif} />
+        <br />
+        <br />
+        <br />
+        <Link to={resultsPath}>
+          <Button
+            id="btnCalculate"
+            handleClick={this.handleCalculation}
+            style={{ borderRadius: 2 }}
+            text="CALCULATE MY WEIRDNESS SCORE" />
+        </Link>
+        <br />
+        <br />
+        <GifsLikedCounter countsLeft={5 - gifsLikedCount} />
+      </div>
+
+    )
+
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    gifsLiked: state.gifs.liked,
+    gifsLikedCount: state.gifs.likedCount
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    {
+      unlikeGif: unlikeGif,
+      subtractScore: subtractScore,
+      calculateScore: calculateScore
+    },
+    dispatch
+  );
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(WeirdnessCalculator);
