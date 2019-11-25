8dfaee3f9181d07b036b79a8ec9e65fc0e03228d
Tayler Johnson <johnsontayler@me.com>
2019-11-25 13:53:04 -0300

    gifconfetti classname

--- src/scenes/Results/components/GifConfetti/index.jsx
+++ src/scenes/Results/components/GifConfetti/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
 const GifConfetti = () => {
+
   return (
     <img
+      className="gif-confetti"
       id="gifConfetti"
       src="https://i.gifer.com/6ob.gif"
       alt="confetti Gif" />
